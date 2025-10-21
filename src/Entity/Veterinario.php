d405807f99fb6cde4802210f5be7426a2baeb846
Pedro <peuhenry2018@gmail.com>
2025-10-21 20:40:24 -0300

    Criando Entidades / Atributos e Metodos

---
+++ src/Entity/Veterinario.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VeterinarioRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: VeterinarioRepository::class)]
+#[ORM\Table(name: 'veterinarios')]
+class Veterinario
+{
+
+    // Atributos da classe
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type:"integer")]
+    private ?int $id = null;
+
+     #[ORM\Column(type:"string", length:255, unique:true)]
+    #[Assert\NotBlank]
+    private string $crmv;
+
+     #[ORM\Column(type:"string", length:255)]
+    #[Assert\NotBlank]
+    private string $nome;
+
+     #[ORM\ManyToMany(targetEntity: Fazenda::class, mappedBy:"veterinarios")]
+     private Collection $fazendas;
+
+
+    // Metódo Construtor
+
+    public function __construct()
+    {
+        $this->fazendas = new ArrayCollection();
+    }
+
+
+    // Getters and Setters
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCrmv(): string
+    {
+        return $this->crmv;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function setCrmv(string $crmv): self
+    {
+        $this->crmv = $crmv;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Fazenda>
+     */
+    public function getFazendas(): Collection
+    {
+        return $this->fazendas;
+
+    }
+
+
+
+}
